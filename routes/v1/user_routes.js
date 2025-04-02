4927e9a7d285d170eb020cf416840aebfcd158b1
afsalmern <afsal@intersmart.in>
2025-04-02 18:45:56 +0530

    added validation in adding disputes

--- routes/v1/user_routes.js
+++ routes/v1/user_routes.js
@@ -3,11 +3,23 @@ const { getHome, updatUserProfile, getUserProfile, getPackages } = require("../.
 const { verifyToken } = require("../../middlewares/auth");
 const { checkIsKycSubmitted, verifyUserExist } = require("../../middlewares/check_user");
 const { checkRole } = require("../../middlewares/role_check");
-const { getRentalHistory, buyItem, rentItem, returnItem, checkIsDeviceValid, addReasonForDispute } = require("../../controllers/rentals/rentals.controller");
-const {upload, uploadComplaints} = require("../../middlewares/multer");
-const { validateKycData, validate, validateKycDataUpdate } = require("../../validators/validators");
+const {
+  getRentalHistory,
+  buyItem,
+  rentItem,
+  returnItem,
+  checkIsDeviceValid,
+  addReasonForDispute,
+} = require("../../controllers/rentals/rentals.controller");
+const { upload, uploadComplaints } = require("../../middlewares/multer");
+const { validateKycData, validate, validateKycDataUpdate, validateDisputeData } = require("../../validators/validators");
 const { uploadKyc, getUserKycDetails, updateKycStatus, updateKyc } = require("../../controllers/kyc/kyc_controller");
-const { addDepositAmount, getDepositHistories, deductDepositAmount, submitWithDrawRequest } = require("../../controllers/payments/payments_controller");
+const {
+  addDepositAmount,
+  getDepositHistories,
+  deductDepositAmount,
+  submitWithDrawRequest,
+} = require("../../controllers/payments/payments_controller");
 const router = express.Router();
 const complaintController = require("../../controllers/complaints/complaints_controller");
 
@@ -23,7 +35,7 @@ router.put("/profile", verifyToken, checkRole("user"), verifyUserExist, upload, 
 
 //Kyc details
 router.get("/kyc-details", verifyToken, getUserKycDetails);
-router.post("/kyc-details", verifyToken,checkIsKycSubmitted, upload, validateKycData, validate, uploadKyc);
+router.post("/kyc-details", verifyToken, checkIsKycSubmitted, upload, validateKycData, validate, uploadKyc);
 router.patch("/kyc-details", verifyToken, upload, validateKycDataUpdate, validate, updateKyc);
 
 //Rental details
@@ -32,8 +44,7 @@ router.post("/buy-item", verifyToken, checkRole("user"), buyItem);
 router.post("/start-rent", verifyToken, checkRole("user"), rentItem);
 router.post("/return-item", verifyToken, checkRole("user"), returnItem);
 router.get("/validate-device", verifyToken, checkRole("user"), checkIsDeviceValid);
-router.post("/add-dispute", verifyToken, checkRole("user"), addReasonForDispute);
-
+router.post("/add-dispute", verifyToken, checkRole("user"), validateDisputeData, validate, addReasonForDispute);
 
 //Transactions
 router.patch("/deposit-amount", verifyToken, checkRole("user"), addDepositAmount);
--- validators/validators.js
+++ validators/validators.js
@@ -105,6 +105,11 @@ const validateKycDataUpdate = [
   body("proof_number").not().isEmpty().withMessage("Proof number is required"),
 ];
 
+const validateDisputeData = [
+  body("rental_id").not().isEmpty().withMessage("Rental id is required"),
+  body("dispute").not().isEmpty().withMessage("Reason is required"),
+];
+
 const validate = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
@@ -127,5 +132,6 @@ module.exports = {
   validateId,
   validateKycData,
   validateKycDataUpdate,
+  validateDisputeData,
   validate,
 };
