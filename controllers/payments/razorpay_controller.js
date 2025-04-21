bd4b10eb14de016d3b7438dd57d2bac6f445fe85
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2025-04-21 14:06:29 +0530

    generate revenew report

--- controllers/payments/razorpay_controller.js
+++ controllers/payments/razorpay_controller.js
@@ -1,61 +1,61 @@
-const RazorPay = require("razorpay");
-const { sendSuccess } = require("../../handlers/success_response_handler");
-const crypto = require('crypto');
+// const RazorPay = require("razorpay");
+// const { sendSuccess } = require("../../handlers/success_response_handler");
+// const crypto = require('crypto');
 
-const razorpay = new RazorPay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET,
-});
+// const razorpay = new RazorPay({
+//   key_id: process.env.RAZORPAY_KEY_ID,
+//   key_secret: process.env.RAZORPAY_KEY_SECRET,
+// });
 
-exports.createOrder = async (req, res, next) => {
-  const { amount } = req.body;
+// exports.createOrder = async (req, res, next) => {
+//   const { amount } = req.body;
 
-  const currency = "INR";
-  const options = {
-    amount: amount * 100,
-    currency: currency,
-    payment_capture: 1,
-  };
+//   const currency = "INR";
+//   const options = {
+//     amount: amount * 100,
+//     currency: currency,
+//     payment_capture: 1,
+//   };
 
-  try {
-    if (!amount || amount <= 0) {
-      throw new Error("Amount should be a valid positive number");
-    }
-    const order = await razorpay.orders.create(options);
-    if (!order) {
-      throw new Error("Order not created");
-    }
+//   try {
+//     if (!amount || amount <= 0) {
+//       throw new Error("Amount should be a valid positive number");
+//     }
+//     const order = await razorpay.orders.create(options);
+//     if (!order) {
+//       throw new Error("Order not created");
+//     }
 
-    console.log(order);
+//     console.log(order);
 
-    const orderDetails = {
-      id: order.id,
-      currency: order.currency,
-      amount: order.amount,
-    };
+//     const orderDetails = {
+//       id: order.id,
+//       currency: order.currency,
+//       amount: order.amount,
+//     };
 
-    sendSuccess(res, "Order created successfully", { order: orderDetails }, 200);
-  } catch (error) {
-    console.error(error);
-    next(error);
-  }
-};
+//     sendSuccess(res, "Order created successfully", { order: orderDetails }, 200);
+//   } catch (error) {
+//     console.error(error);
+//     next(error);
+//   }
+// };
 
-exports.verifyOrder = async (req, res, next) => {
-  try {
-    const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
+// exports.verifyOrder = async (req, res, next) => {
+//   try {
+//     const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
 
-    const sign = razorpay_order_id + "|" + razorpay_payment_id;
+//     const sign = razorpay_order_id + "|" + razorpay_payment_id;
 
-    const expectedSignature = crypto.createHmac("sha256", razorpay.key_secret).update(sign.toString()).digest("hex");
+//     const expectedSignature = crypto.createHmac("sha256", razorpay.key_secret).update(sign.toString()).digest("hex");
 
-    if (razorpay_signature === expectedSignature) {
-      sendSuccess(res, "Order verified successfully", {}, 200);
-    } else {
-      throw new Error("Order verification failed");
-    }
-  } catch (error) {
-    console.log("error verifiying order", error);
-    next(error);
-  }
-};
+//     if (razorpay_signature === expectedSignature) {
+//       sendSuccess(res, "Order verified successfully", {}, 200);
+//     } else {
+//       throw new Error("Order verification failed");
+//     }
+//   } catch (error) {
+//     console.log("error verifiying order", error);
+//     next(error);
+//   }
+// };
--- routes/v1/web_routes.js
+++ routes/v1/web_routes.js
@@ -40,7 +40,7 @@ const { getDashboard, getYearWiseData } = require("../../controllers/web/dashboa
 router.get("/dashboard", verifyToken, checkRole("admin"), getDashboard);
 router.get("/year-wise-data", verifyToken, checkRole("admin"), getYearWiseData);
 
-const { generateRentalReport, generateLocationsReport } = require("../../controllers/web/reports_controller");
+const { generateRentalReport, generateLocationsReport, generateRevenewReport } = require("../../controllers/web/reports_controller");
 
 //Checks and deposit deposit_amount
 router.get("/checks-and-amount", verifyToken, checkRole("admin"), getChecksAndAmount);
@@ -101,6 +101,7 @@ router.put("/powerbank/:id", powerbankController.updatePowerBank);
 router.delete("/powerbank/:id", powerbankController.deletePowerBank);
 
 router.get("/rental-report", generateRentalReport);
+router.get("/revenew-report", generateRevenewReport);
 router.get("/location-report", generateLocationsReport);
 
 module.exports = router;
