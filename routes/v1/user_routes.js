284b645bb9cf4ed674ff6206dd48a3e4b20ee596
afsalmern <afsal@intersmart.in>
2025-10-06 19:00:12 +0530

    bug fix

--- routes/v1/user_routes.js
+++ routes/v1/user_routes.js
@@ -58,6 +58,7 @@ router.put("/verify-rental-otp", verifyToken, checkRole("user"), verfiyRentalsOt
 router.post("/test", verifyToken, checkRole("user"), test);
 
 // //Payments
+router.post("/create-order", verifyToken, checkRole("user"), createOrder);
 router.post("/verify-order", verifyToken, checkRole("user"), verifyOrder);
 
 //Transactions
