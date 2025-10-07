7f664d73a388ef821f113ce2ebb5b2ce8684ea1a
afsalmern <afsal@intersmart.in>
2025-10-07 18:44:51 +0530

    bufg fix

--- helpers/rentalsHelper.js
+++ helpers/rentalsHelper.js
@@ -140,11 +140,10 @@ const startRent = async (user_id, box_id, package_id, order_id) => {
 
 const updateRentalPaymentStatus = async (database, payload, status, type = "default") => {
   try {
-    const test = database.findAll();
-    console.log("test", test);
-
     const paymentEntity = payload.payment.entity;
     const order_id = paymentEntity.order_id;
+    const datas = await database.findAll();
+    console.log("datas", datas);
     const paymentsData = await database.findOne({ where: { order_id } });
     await paymentsData.update({ status });
 
