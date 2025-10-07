b7a75920843b71b4cdf5506c41c77b456096294d
afsalmern <afsal@intersmart.in>
2025-10-07 11:57:27 +0530

    bug fix

--- controllers/payments/razorpay_controller.js
+++ controllers/payments/razorpay_controller.js
@@ -2,7 +2,7 @@ const { sendSuccess } = require("../../handlers/success_response_handler");
 const { startRent, initiateRefund, addDepositAmount, revertDepositAmount } = require("../../helpers/rentalsHelper");
 const db = require("../../models");
 const { initiateOrder, verifySignature } = require("../../helpers/razorPayHelpers");
-const { ApiError } = require("../../middlewares/error");
+const crypto = require("crypto");
 
 const ChecksAndAmount = db.checks_and_amounts;
 
