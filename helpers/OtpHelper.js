27c38f3723267f435317e0755e59b943fecd4e37
afsalmern <afsal@intersmart.in>
2025-10-08 19:26:42 +0530

    bug fix in otp

--- helpers/OtpHelper.js
+++ helpers/OtpHelper.js
@@ -2,7 +2,6 @@ const { default: axios } = require("axios");
 
 const sendOtp = async (otp, number) => {
   console.log("Sending OTP:", otp, "to numbers:", number);
-  return true;
 
   try {
     const smsResponse = await axios.post(
