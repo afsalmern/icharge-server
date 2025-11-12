382c38959eff30b037c5460961449a80eb36c43f
afsalmern <afsal@intersmart.in>
2025-11-12 14:47:42 +0530

    bug fix

--- controllers/rentals/rentals.controller.js
+++ controllers/rentals/rentals.controller.js
@@ -63,7 +63,7 @@ exports.checkIsDeviceValid = async (req, res, next) => {
         },
       });
 
-      isCodeUsed = existingUserReferel;
+      isCodeUsed = existingUserReferel ? true : false;
     }
 
     // If box exists and has available powerbanks, return success
