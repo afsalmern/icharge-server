2aadcae0a313d40af3da4d81213409111b95f2e0
afsalmern <afsal@intersmart.in>
2025-11-26 10:35:40 +0530

    otp enabled

--- helpers/OtpHelper.js
+++ helpers/OtpHelper.js
@@ -4,7 +4,7 @@ const { ApiError } = require("../middlewares/error");
 const sendOtp = async (otp, number) => {
   console.log("Sending OTP:", otp, "to number:", number);
 
-  return true;
+  // return true;
 
   const countryCode = process.env.COUNTRY_CODE;
 
