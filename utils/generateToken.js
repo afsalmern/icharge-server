8c9cd19a4d81c9fee6e4c71718d271d87c0fb62c
afsalmern <afsal@intersmart.in>
2026-04-21 10:05:59 +0530

    extended toke duration to 30 days

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -3,6 +3,6 @@ const secret = process.env.JWT_SECRET;
 exports.generateToken = (user) => {
   const { id, role } = user;
 
-  const token = jwt.sign({ user_id: id, role }, process.env.JWT_SECRET, { expiresIn: "1d" });
+  const token = jwt.sign({ user_id: id, role }, process.env.JWT_SECRET, { expiresIn: "30d" });
   return token;
 };
